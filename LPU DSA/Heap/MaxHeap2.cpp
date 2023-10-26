79eae9a5539d63a30bffd78307f634fbd3c4f0ae
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2023-10-26 10:44:19 +0530

    maxHeapSelf

---
+++ LPU DSA/Heap/MaxHeap2.cpp
@@ -0,0 +1,47 @@
+// Create a maxHeap by taking starting index as 0;
+
+#include<iostream>
+using namespace std;
+
+void maxHeap(int arr[], int k, int size){
+    int val = k;
+    int leftChild = 2*k+1;
+    int rightChild = 2*k+2;
+    if(arr[leftChild]>arr[val]){
+        swap(arr[leftChild], arr[k]);
+    }
+    if(arr[rightChild]>val){
+        swap(arr[rightChild], arr[k]);
+    }
+    else{
+        return;
+    }
+
+}
+
+void build_heap(int arr[], int size){
+    int k;
+    for(k =(size/2)-1; k>=0; k--){
+        maxHeap(arr, k, size);
+    }
+
+}
+
+//23 43 56 21 45 --> 56 23 45 21 43
+
+int main()
+{
+    int a[30], n;
+    cout<<"How many nodes you want to create: ";
+    cin>>n;
+    cout<<"Enter all the elements: ";
+    for(int i = 0; i<n; i++){
+        cin>>a[i];
+    }
+    build_heap(a, n);
+    cout<<"Max Heap is: ";
+    for(int i = 0; i<n; i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
